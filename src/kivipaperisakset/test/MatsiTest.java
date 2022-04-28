5386a0961c8b1063c55bfbd5acf870e6e4a50977
Arttu <arttu.peramaki@gmail.com>
2022-04-28 03:26:23 +0300

    Testejä

--- src/kivipaperisakset/test/MatsiTest.java
+++ src/kivipaperisakset/test/MatsiTest.java
@@ -1,5 +1,7 @@
-package kivipaperisakset;
+package kivipaperisakset.test;
 
+import kivipaperisakset.Matsi;
+import kivipaperisakset.Pelaaja;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
